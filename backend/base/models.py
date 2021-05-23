96dfb5e3b12266d8bf9dbc56f537ab8a92d35568
Jose Algorta <jpalgort@gmail.com>
2021-05-23 20:10:59 -0300

    (BE): Make rating not null in Review model.

--- backend/base/models.py
+++ backend/base/models.py
@@ -23,8 +23,9 @@ class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='reviews')
     name = models.CharField(max_length=200, null=True, blank=True)
-    rating = models.IntegerField(default=0, null=True, blank=True)
+    rating = models.IntegerField(default=0)
     comment = models.TextField(null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.rating)
