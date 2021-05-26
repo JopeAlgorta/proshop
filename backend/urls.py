cfce8505de8dd69bbed326af7dfa14f7c10aaf93
Jose Algorta <jpalgort@gmail.com>
2021-05-26 20:13:58 -0300

    Hot fixes to fix issue in prod.

--- backend/urls.py
+++ backend/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='build/index.html')),
     path('admin/', admin.site.urls),
     path('api/users/', include('base.urls.users')),
     path('api/orders/', include('base.urls.orders')),
