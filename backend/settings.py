32d2d2da137dbfa7da787b0e6052035919205077
Jose Algorta <jpalgort@gmail.com>
2021-05-26 19:40:06 -0300

    Hot fixes to fix issue in prod.

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import environ
+import os
 
 # Initialise environment variables
 env = environ.Env()
@@ -47,8 +48,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'storages',
 
-    'base.apps.BaseConfig',
-    'frontend'
+    'base.apps.BaseConfig'
 ]
 
 REST_FRAMEWORK = {
@@ -105,7 +105,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'frontend/build/'],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
